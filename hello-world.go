d09c7ffdd8ebaa39fcfa46f22ca3ab3ae53b219d
wintun-devop <wintun.hlaing@outlook.com>
2025-08-02 00:30:54 +0630

    init

---
+++ hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// single line commant
+	fmt.Println("Hello, World!")
+	/*
+		    fmt.Println("Hello, World!2")
+			fmt.Println("Hello, World!3")
+	*/
+
+}
