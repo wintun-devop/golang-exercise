fd976735d50835b4c443f114d4f0814170ca18d7
wintun-devop <wintun.hlaing@outlook.com>
2025-08-03 17:13:04 +0630

    update

--- myapp/main.go
+++ myapp/main.go
@@ -12,8 +12,27 @@ func main() {
 	fmt.Printf("Welcome to Our %v App.\n", conferenceName)
 	fmt.Printf("We have total of %v and %v remaining.\n", conferenceTickets, remainTickests)
 	fmt.Println("Get your ticket and go confrence.")
+	fmt.Printf("conferenceName is %T and conferenceTickets is %T.\n", conferenceName, conferenceTickets)
+
+	var userName string
+	var userEmaail string
+	var bookings [50]string
+	// user input
+	fmt.Printf("userName:\n")
+	fmt.Scan(&userName)
+	fmt.Printf("userEmail:\n")
+	fmt.Scan(&userEmaail)
+
+	// print out
+	fmt.Printf("%v\n", userName)
+	fmt.Printf("%v\n", userEmaail)
+	fmt.Printf("%v\n", bookings)
+	fmt.Printf("%T\n", bookings)
+
+	// calling outside function
+	executed()
 }
 
-func myMessage() {
+func executed() {
 	fmt.Println("I just got executed!")
 }
